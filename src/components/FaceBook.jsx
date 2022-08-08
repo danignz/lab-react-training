03e492794e18509f78a5a12bff81c4181dc8acc8
Daniel Gonzalez <danielgnzch@gmail.com>
2022-08-08 10:40:23 +0200

    Iteration 14 done without bonus

--- src/components/FaceBook.jsx
+++ src/components/FaceBook.jsx
@@ -1,16 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import profiles from '../data/berlin.json';
 
 export default function FaceBook() {
+  const [profilesData, setProfiles] = useState(profiles);
+
   const typeOfUser = (userType) => {
     return userType === true ? 'Student' : 'Teacher';
   };
 
+  const uniqueCountries = [];
+
+  profiles.forEach((member) => {
+    if (member.isStudent) {
+      const isDuplicate = uniqueCountries.includes(member.country);
+      if (!isDuplicate) {
+        uniqueCountries.push(member.country);
+      }
+    }
+  });
+
+  const handleSelection = (country) => {
+    const profilesSelected = profiles.map((profile) => {
+      let isSelected;
+      if (country === 'All' && profile.isStudent) {
+        isSelected = true;
+      } else {
+        isSelected =
+          country === profile.country && profile.isStudent ? true : false;
+      }
+
+      return {
+        ...profile,
+        selected: isSelected,
+      };
+    });
+    setProfiles(profilesSelected);
+  };
+
   return (
     <div>
-      {profiles.map((user, index) => {
+      <div id="countryBtn">
+        <button onClick={() => handleSelection('All')}>All</button>
+        {uniqueCountries.map((country) => (
+          <button onClick={() => handleSelection(country)} key={country}>
+            {country}
+          </button>
+        ))}
+      </div>
+
+      {profilesData.map((user, index) => {
         return (
-          <div className="card-container" key={index}>
+          <div
+            className={`card-container ${user.selected ? 'blue' : 'white'}`}
+            key={index}
+          >
             <img
               width="150px"
               src={user.img}
