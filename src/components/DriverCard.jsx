651f334265174d0d80813a875bb176a55a05e9d4
Daniel Gonzalez <danielgnzch@gmail.com>
2022-08-09 13:07:46 +0200

    Refactor Iteration 6 & 7

--- src/components/DriverCard.jsx
+++ src/components/DriverCard.jsx
@@ -1,38 +1,15 @@
 import React from 'react';
+import Rating from './Rating';
 
 export default function DriverCard(props) {
   const { name, rating, img, car } = props;
-  const roundedRating = Math.round(rating);
-  let starTemplate = '';
-
-  switch (true) {
-    case roundedRating < 1:
-      starTemplate = '☆ ☆ ☆ ☆ ☆';
-      break;
-    case 1 <= roundedRating && roundedRating < 2:
-      starTemplate = '★ ☆ ☆ ☆ ☆';
-      break;
-    case 2 <= roundedRating && roundedRating < 3:
-      starTemplate = '★ ★ ☆ ☆ ☆';
-      break;
-    case 3 <= roundedRating && roundedRating < 4:
-      starTemplate = '★ ★ ★ ☆ ☆';
-      break;
-    case 4 <= roundedRating && roundedRating < 5:
-      starTemplate = '★ ★ ★ ★ ☆';
-      break;
-    case 5 <= roundedRating:
-      starTemplate = '★ ★ ★ ★ ★';
-      break;
-    default:
-  }
 
   return (
     <div className="DriverCard">
       <img src={img} alt="Pic of user" />
       <div>
         <h2>{name}</h2>
-        <p>{starTemplate}</p>
+        <Rating>{rating.toString()}</Rating>
         <p>
           {car.model} - {car.licensePlate}
         </p>
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -4,29 +4,34 @@ export default function Rating(props) {
   const { children } = props;
   const parseChildren = Math.round(parseFloat(children));
 
-  let starTemplate = '';
+  const starTemplate = ['☆', '☆', '☆', '☆', '☆'];
 
-  switch (true) {
-    case parseChildren < 1:
-      starTemplate = '☆ ☆ ☆ ☆ ☆';
-      break;
-    case 1 <= parseChildren && parseChildren < 2:
-      starTemplate = '★ ☆ ☆ ☆ ☆';
-      break;
-    case 2 <= parseChildren && parseChildren < 3:
-      starTemplate = '★ ★ ☆ ☆ ☆';
-      break;
-    case 3 <= parseChildren && parseChildren < 4:
-      starTemplate = '★ ★ ★ ☆ ☆';
-      break;
-    case 4 <= parseChildren && parseChildren < 5:
-      starTemplate = '★ ★ ★ ★ ☆';
-      break;
-    case 5 <= parseChildren:
-      starTemplate = '★ ★ ★ ★ ★';
-      break;
-    default:
+  for (let i = 0; i < parseChildren; i++) {
+    starTemplate[i] = '★';
   }
 
-  return <div>{starTemplate}</div>;
+  // let starTemplate = '';
+  // switch (true) {
+  //   case parseChildren < 1:
+  //     starTemplate = '☆ ☆ ☆ ☆ ☆';
+  //     break;
+  //   case 1 <= parseChildren && parseChildren < 2:
+  //     starTemplate = '★ ☆ ☆ ☆ ☆';
+  //     break;
+  //   case 2 <= parseChildren && parseChildren < 3:
+  //     starTemplate = '★ ★ ☆ ☆ ☆';
+  //     break;
+  //   case 3 <= parseChildren && parseChildren < 4:
+  //     starTemplate = '★ ★ ★ ☆ ☆';
+  //     break;
+  //   case 4 <= parseChildren && parseChildren < 5:
+  //     starTemplate = '★ ★ ★ ★ ☆';
+  //     break;
+  //   case 5 <= parseChildren:
+  //     starTemplate = '★ ★ ★ ★ ★';
+  //     break;
+  //   default:
+  // }
+
+  return <p>{starTemplate}</p>;
 }
