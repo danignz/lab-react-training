1f783178a195e53bdf4f9f23a390074f3235c2a2
Daniel Gonzalez <danielgnzch@gmail.com>
2022-08-16 18:17:07 +0200

    Lab DONE! Iteration 15 bonus done

--- src/components/SignupPage.jsx
+++ src/components/SignupPage.jsx
@@ -9,7 +9,37 @@ export default function SignupPage(props) {
     nationality: '',
   });
 
+  const [passwordIsValid, setPassword] = useState(false);
+  const [emailIsValid, setEmail] = useState(false);
+
+  const handleEmail = (email) => {
+    const regex =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!regex.test(email)) {
+      setEmail(false);
+    } else {
+      setEmail(true);
+    }
+  };
+
+  const handlePassword = (password) => {
+    const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    if (!regex.test(password)) {
+      setPassword(false);
+    } else {
+      setPassword(true);
+    }
+  };
+
   const handleChange = (e) => {
+    if (e.target.name === 'email') {
+      handleEmail(e.target.value);
+    }
+
+    if (e.target.name === 'password') {
+      handlePassword(e.target.value);
+    }
+
     setUser((prev) => {
       return {
         ...prev,
@@ -20,13 +50,17 @@ export default function SignupPage(props) {
 
   const handleForm = (e) => {
     e.preventDefault();
-    newSignUp(user, true);
 
-    setUser({
-      email: '',
-      password: '',
-      nationality: '',
-    });
+    if (passwordIsValid && emailIsValid) {
+      newSignUp(user, true);
+      setUser({
+        email: '',
+        password: '',
+        nationality: '',
+      });
+    } else {
+      alert('Cannot login!! Check email and password requirements');
+    }
   };
 
   return (
@@ -40,6 +74,12 @@ export default function SignupPage(props) {
           value={user.email}
           onChange={(e) => handleChange(e)}
         />
+        {user.email.length >= 1 &&
+          (emailIsValid ? (
+            <p style={{ color: 'green' }}>You typed a valid email</p>
+          ) : (
+            <p style={{ color: 'red' }}>You typed a invalid email</p>
+          ))}
         <label>Password</label>
         <input
           type="password"
@@ -48,6 +88,12 @@ export default function SignupPage(props) {
           value={user.password}
           onChange={(e) => handleChange(e)}
         />
+        {user.password.length >= 1 &&
+          (passwordIsValid ? (
+            <p style={{ color: 'green' }}>You typed a strong password</p>
+          ) : (
+            <p style={{ color: 'red' }}>Your password is too weak</p>
+          ))}
         <label>Nationality</label>
         <select
           value={user.nationality}
